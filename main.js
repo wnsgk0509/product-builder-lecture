bb3cf134f39ebafed82da99241926f7b201921bb
rndrnd7qkd <rndrnd7qkd@gmail.com>
2026-02-17 06:14:12 +0000

    Initial commit

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-// Add JS here
+
+document.getElementById('generate-btn').addEventListener('click', () => {
+    const numbers = new Set();
+    while (numbers.size < 6) {
+        numbers.add(Math.floor(Math.random() * 45) + 1);
+    }
+    const sortedNumbers = Array.from(numbers).sort((a, b) => a - b);
+    const numberElements = document.querySelectorAll('.number');
+    numberElements.forEach((el, index) => {
+        el.textContent = sortedNumbers[index];
+    });
+});
